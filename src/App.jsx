204fc957ac7470a76034d7e362dda64bad77bb46
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-11 12:42:13 +0200

    Add new components and assets for the shop feature
    - Implemented Hero component with styling and layout.
    - Created Item component as a placeholder for individual items.
    - Developed Navbar component with navigation links and cart functionality.
    - Added Cart, LoginSignup, Product, Shop, and ShopCategory pages as placeholders.
    - Introduced new assets including icons and images for the frontend.
    - Styled components with CSS for layout and design consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/men' element={<ShopCategory category="men" />} />
+        <Route path='/women' element={<ShopCategory category="women" />} />
+        <Route path='/kids' element={<ShopCategory category="kids" />} />
+        <Route path='/product' element={<Product />} >
+          <Route path=':productId' element={<Product />} />
+        </Route>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero/Hero";
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Shop;
