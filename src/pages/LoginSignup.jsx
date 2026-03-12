5eb852fcdefdfb355743c003af3fde59ad4ae18b
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-12 08:20:00 +0200

    Add LoginSignup component with styles for user registration

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,7 +1,22 @@
+import './css/LoginSignup.css';
+
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">ALready have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the Terms of Use & the Provacy Policy</p>
+        </div>
+      </div>
     </div>
   );
 }
