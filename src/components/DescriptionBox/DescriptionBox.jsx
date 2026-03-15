813df6fd55c2f4c0fc80a2fd5eab72a87cc6695d
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-15 15:32:13 +0200

    Add DescriptionBox component and integrate it into the Product page

---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum saepe ex esse in, inventore corporis nobis eaque omnis non minima explicabo repellat autem aspernatur debitis veniam quisquam neque nemo. Quam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, earum suscipit eum deserunt beatae architecto alias tempore vitae corporis ab nesciunt quam et dolorem delectus odio non soluta officia aut.</p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/ShopContext';
 import { useParams } from "react-router-dom";
 import Breadcrum from "../components/Breadcrums/Breadcrum";
 import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -18,6 +19,7 @@ const Product = () => {
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   );
 }
