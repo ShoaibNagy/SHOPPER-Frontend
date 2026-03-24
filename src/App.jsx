18e4df01793aa23eadd327af76907ded0bcf577e
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-24 10:11:13 +0200

    Remove unused App.css import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Shop from './pages/Shop'
 import ShopCategory from './pages/ShopCategory';
