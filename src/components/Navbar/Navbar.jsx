01bf48ffb5a4f81269934bfd9bbe7cd2f5cdcb63
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-25 19:30:16 +0200

    Refactor Navbar component for improved readability and update LoginSignup component with login/signup functionality and styling enhancements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import nav_dropdown from '../../assets/Frontend_Assets/nav_dropdown.png';
 const Navbar = () => {
 
   const [menu, setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext);
+  const { getTotalCartItems } = useContext(ShopContext);
   const menuRef = useRef();
 
   const dropdown_toggle = (e) => {
@@ -26,22 +26,29 @@ const Navbar = () => {
 
       <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
-        <li onClick={() => {setMenu("shop")}}>
+        <li onClick={() => { setMenu("shop") }}>
           <Link style={{ textDecoration: 'none' }} to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}
         </li>
-        <li onClick={() => {setMenu("men")}}>
+        <li onClick={() => { setMenu("men") }}>
           <Link style={{ textDecoration: 'none' }} to="/men">Men</Link>{menu === "men" ? <hr /> : <></>}
         </li>
-        <li onClick={() => {setMenu("women")}}>
+        <li onClick={() => { setMenu("women") }}>
           <Link style={{ textDecoration: 'none' }} to="/women">Women</Link>{menu === "women" ? <hr /> : <></>}
         </li>
-        <li onClick={() => {setMenu("kids")}}>
+        <li onClick={() => { setMenu("kids") }}>
           <Link style={{ textDecoration: 'none' }} to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
 
       <div className="nav-login-cart">
-        <Link to="/login"><button>Login</button></Link>
+        {localStorage.getItem('auth-token') ?
+          <button onClick={() => {
+            localStorage.removeItem('auth-token');
+            window.location.replace('/')
+          }}>Logout</button>
+          :
+          <Link to="/login"><button>Login</button></Link>}
+
         <Link to="/cart"><img src={cart_icon} alt="Cart Icon" /></Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,20 +1,75 @@
+import { useState } from 'react';
 import './css/LoginSignup.css';
 
 const LoginSignup = () => {
+  const [state, setState] = useState('Sign Up');
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const login = async () => {
+    console.log("Login Funciton Executed", formData);
+    let responseData;
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => { response.json() }).then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  }
+
+  const signup = async () => {
+    console.log("Sign Up Function Eecuted", formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => { response.json() }).then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  }
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state === 'Sign Up' ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Full Name" required /> : <></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" required />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Password" required />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">ALready have an account? <span>Login here</span></p>
+        <button onClick={() => { state === 'Login' ? login() : signup() }}>Continue</button>
+        {state === 'Sign Up' ?
+          <p className="loginsignup-login">ALready have an account? <span onClick={() => { setState('Login') }}>Login here</span></p>
+          :
+          <p className="loginsignup-signup">New to SHOPPER? <span onClick={() => { setState('Sign Up') }}>Sign Up here</span></p>}
         <div className="loginsignup-agree">
-          <input type="checkbox" name="" id="" />
-          <p>By continuing, I agree to the Terms of Use & the Provacy Policy</p>
+          {state === 'Sign Up' ? <><input type="checkbox" name="" id="" />
+            <p>By continuing, I agree to the Terms of Use & the Provacy Policy</p></> : <></>}
         </div>
       </div>
     </div>
