3d85277f45b752d8accd260ffd94cbd9bc7e45da
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-25 21:41:46 +0200

    Add data fetching for new collections and update component state

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import new_collections from "../../assets/Frontend_Assets/new_collections";
 import Item from "../Item/Item";
 import './NewCollections.css';
 
 const NewCollections = () => {
+  const [new_collection, setNewCollection] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:4000/newcollections')
+      .then((response) => response.json())
+      .then((data) => setNewCollection(data));
+  }, []);
+
   return (
     <div className="new-collections">
       <h1>NEW COLLECTIONS</h1>
@@ -11,7 +21,7 @@ const NewCollections = () => {
         {new_collections.map((item, i) => {
           return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
-      </div> 
+      </div>
     </div>
   )
 }
