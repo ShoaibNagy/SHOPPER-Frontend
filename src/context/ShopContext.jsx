e1446e23041a08f412560cc3fb447c16c78eda7e
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-25 21:41:29 +0200

    Refactor ShopContext to improve cart functionality and integrate product fetching

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import all_product from "../assets/Frontend_Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length + 1; index++) {
+  for (let index = 0; index < 300 + 1; index++) {
     cart[index] = 0;
   }
   return cart;
@@ -13,39 +13,47 @@ const getDefaultCart = () => {
 
 const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [all_products, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/allproducts')
+      .then((response) => response.json())
+      .then((data) => { setAllProducts(data) });
+  }, []);
 
   const addToCart = (itemId) => {
-    setCartItems((prev) => ({...prev, [itemId]:prev[itemId] + 1}));
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     console.log(cartItems0);
   };
 
   const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({...prev, [itemId]:prev[itemId] - 1}))
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
   };
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for(const item in cartItems){
-      if(cartItems[item] > 0){
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
         let itemInfo = all_product.find((product) => product.id === Number(item));
         totalAmount += itemInfo.new_price * cartItems[item];
       }
     }
 
+    console.log(all_products);
     return totalAmount;
   };
 
   const getTotalCartItems = () => {
     let totalItems = 0;
-    for(const item in cartItems){
-      if(cartItems[item] > 0){
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
         totalItems += cartItems[item];
       }
     }
-    
+
     return totalItems;
   };
-  
+
   const contextValue = {
     all_product,
     cartItems,
